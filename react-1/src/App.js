74ab1b5d6e790107f51165e3f1ef0ab142e266fc
ZonezIpex <aksvmfkd3@naver.com>
2025-04-03 19:33:37 +0900

    0403 마무리 커밋

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,31 +1,59 @@
+import { useState } from 'react';
+import MyB from './MyButton';
 import { Button1, Button3 } from './ButtonLib';
 import AboutPage from './AboutPage';
 import Profile from './Profile';
-import './App.css';
 import ShoppingList from './ShoppingList';
 import CountState from './CountState';
+import './App.css';
 
-function MyButton() {
+function CountState2({ count, onClick }) {
   return (
-    <button>I'm a button</button>
+    <div>
+      <button onClick={onClick}>Clicked {count} times</button>
+    </div>
   );
 }
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="wrapper">
-      <div>
       <h1>Welcome to my app</h1>
-      <MyButton />
-      <Button1 />
-      <Button3 />
-      <AboutPage />
-      <Profile />
-      <ShoppingList />
-      </div>
+
       <div>
-      <CountState />
-      <CountState />
+        <p>default export example</p>
+        <MyB />
+
+        <p>named export example</p>
+        <Button1 /> &nbsp; <Button3 />
+
+        <p>wrapping example</p>
+        <AboutPage />
+
+        <p>Displaying data</p>
+        <Profile />
+
+        <p>Rendering lists</p>
+        <ShoppingList />
+      </div>
+
+      <div>
+        <p>Updating the screen</p>
+        <CountState />
+        <CountState />
+        <CountState />
+      </div>
+
+      <div>
+        <p>Sharing data between components</p>
+        <CountState2 count={count} onClick={handleClick} />
+        <CountState2 count={count} onClick={handleClick} />
       </div>
     </div>
   );
